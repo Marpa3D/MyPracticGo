7820dc67cb02e08b7ca4b0490e7a2cc1d5712971
Marpa3D <marpa3d.evangelist@gmail.com>
2021-09-22 08:16:30 +0300

    Создан и использован интерфейс

--- cmd/interfaces/main.go
+++ cmd/interfaces/main.go
@@ -1,1 +1,40 @@
 package main
+
+import "log"
+
+type Animal interface {
+	Says() string
+	NumberOfLegs() int
+}
+
+type Dog struct {
+	Name string
+	Breed string
+}
+
+type Gorilla struct {
+	Name string
+	Color string
+	NumberOfTeeth int
+}
+
+func main()  {
+	dog := Dog{
+		Name: "Bublik",
+		Breed: "Buldog",
+	}
+
+	PrintInfo(dog)
+}
+
+func (d Dog) Says() string {
+	return "Gaaf!)"
+}
+
+func (d Dog) NumberOfLegs() int {
+	return 4
+}
+
+func PrintInfo(a Animal)  {
+	log.Println("Это животное говорит:", a.Says(), "И у него", a.NumberOfLegs(), "ноги.")
+}
