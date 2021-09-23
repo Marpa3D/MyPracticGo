b79e47a21b2762d4327dddd0c12ddfaca444d8cb
Marpa3D <marpa3d.evangelist@gmail.com>
2021-09-23 09:06:06 +0300

    Реорганизация структуры

--- main.go
+++ main.go
@@ -1,24 +1,5 @@
 package main
 
-import (
-	"MyPracticGo/cmd/channels"
-	"log"
-)
-
-const numPool = 1024
-
-func CalcuateValues(inChan chan int)  {
-	randomNumber := channels.RandomNumbers(numPool)
-	inChan <- randomNumber
-}
-
 func main()  {
-
-	inChan := make(chan int)
-	defer close(inChan)
-
-	go CalcuateValues(inChan)
-	num := <- inChan
-
-	log.Println(num)
+	
 }
